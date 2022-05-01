5b6c3e0e91ced39efc9cb1b83e4c7b795f7073eb
majdhasan <majd.hasan@hotmail.com>
2022-05-01 17:18:26 +0200

    refactor

--- src/main/java/life/majd/backend/repository/postgres/EventRepository.java
+++ src/main/java/life/majd/backend/repository/postgres/EventRepository.java
@@ -1,17 +1,17 @@
 package life.majd.backend.repository.postgres;
 
+import java.util.Set;
 import life.majd.backend.repository.postgres.entity.EventEntity;
 import life.majd.backend.repository.postgres.entity.EventLocationEntity;
 import life.majd.backend.repository.postgres.entity.PersonEntity;
-import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
 
-  Set<EventEntity> findAllByOrganizer(PersonEntity person);
-  Set<EventEntity> findAllByLocation(EventLocationEntity location);
+  Set<EventEntity> findEventEntitiesByOrganizer(PersonEntity person);
 
+  Set<EventEntity> findEventEntitiesByLocation(EventLocationEntity location);
 
 }
